d4f7adfa7d84e8473a590bb02ff705413f9902a7
Adair Castillo <adair.castillo@dexcom.com>
2022-06-29 19:08:19 -0500

    View binding activated

--- app/src/main/java/net/nemesis/fragments/MainActivity.kt
+++ app/src/main/java/net/nemesis/fragments/MainActivity.kt
@@ -2,10 +2,15 @@ package net.nemesis.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.nemesis.fragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
